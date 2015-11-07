24b2ed0d65e2890472a53124596639677a4ce908
Moritz Schulze <moritz@frontierpsychiatrist.de>
2015-11-07 11:19:22 +0100

    Ex functions-3

--- functions/functions-2.exs
+++ functions/functions-2.exs
@@ -10,3 +10,13 @@ IO.puts fizz.(0, 0, 3)
 IO.puts fizz.(0, 1, 2)
 IO.puts fizz.(:a, 0, 3)
 IO.puts fizz.(1, 2, :a)
+
+fizzbuzz = fn n -> fizz.(rem(n, 3), rem(n, 5), n) end
+
+IO.puts fizzbuzz.(10)
+IO.puts fizzbuzz.(11)
+IO.puts fizzbuzz.(12)
+IO.puts fizzbuzz.(13)
+IO.puts fizzbuzz.(14)
+IO.puts fizzbuzz.(15)
+IO.puts fizzbuzz.(16)
