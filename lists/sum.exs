77495f450c08efa64cd5e081062732bca7c93184
Moritz Schulze <moritz@frontierpsychiatrist.de>
2015-11-07 13:40:02 +0100

    Ex max list function

--- lists/sum.exs
+++ lists/sum.exs
@@ -1,6 +1,7 @@
 defmodule Lists do
 	def sum([]), do: 0
 	def sum([head | tail]), do: head + sum(tail)
-end
 
-	
+	def max([x]), do: x
+	def max([ head | tail ]), do: Kernel.max(head, max(tail))
+end
