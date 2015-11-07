eb5c2e8b8b09966b8a016877b6638697d430ec39
Moritz Schulze <moritz@frontierpsychiatrist.de>
2015-11-07 18:16:39 +0100

    Ex span list

--- lists/sum.exs
+++ lists/sum.exs
@@ -4,4 +4,15 @@ defmodule Lists do
 
 	def max([x]), do: x
 	def max([ head | tail ]), do: Kernel.max(head, max(tail))
+
+	def caesar(charlist, n), do: Enum.map(charlist, &(shift_char(n, &1)))
+ 
+	defp shift_char(n, c) when n + c <= ?z, do: n+c
+	defp shift_char(n, c), do: n + c - 26
+
+	def span(from, to), do: _span(from, to, [])
+
+	defp _span(from, to, acc) when from == to, do: [ to | acc ]
+	defp _span(from, to, acc) when from < to, do: _span(from, to - 1, [ to | acc ])
+																 
 end
